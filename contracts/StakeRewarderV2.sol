707fefb4f192f91226497543989f1ff3a5f2a4d4
Tony Zhou <tony.zhou.vue@gmail.com>
2021-07-20 09:48:56 -0700

    created uncompleted StakeRewarderV2 combined one, add solhint, prettier rules, hardhat ts config, updated sol version.

--- contracts/StakeRewarderV2.sol
+++ contracts/StakeRewarderV2.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./VestingMultiVault.sol";
+import "./interfaces/IVestingVault.sol";
 
 /**
  * @title StakeRewarder
@@ -13,7 +14,7 @@ import "./VestingMultiVault.sol";
  * It's based on Sushi's MasterChef v1, but notably only serves what's already
  * available: no new tokens can be created. It's just a restaurant, not a farm.
  */
-contract StakeRewarder is Ownable {
+contract StakeRewarderV2 is Ownable, AccessControl, IVestingVault {
 	using SafeMath for uint256;
 	using SafeERC20 for IERC20;
 
@@ -65,6 +66,18 @@ contract StakeRewarder is Ownable {
 	// The block number when staking starts.
 	uint256 public startBlock;
 
+	// The token being vested.
+	IERC20 public immutable token;
+
+	// The amount unclaimed for an address, whether or not vested.
+	mapping(address => uint256) public pendingAmount;
+
+	// The allocations assigned to an address.
+	mapping(address => Allocation[]) public userAllocations;
+
+	// The precomputed hash of the "ISSUER" role.
+	bytes32 public constant ISSUER = keccak256("ISSUER");
+
 	event TokenAdded(
 		address indexed token,
 		uint256 weight,
@@ -96,7 +109,8 @@ contract StakeRewarder is Ownable {
 		uint256 _startBlock,
 		uint256 _vestingCliff,
 		uint256 _vestingDuration,
-		VestingMultiVault _vault
+		VestingMultiVault _vault,
+		IERC20 _token
 	) {
 		// Set the initial reward config
 		rewardPerBlock = _rewardPerBlock;
@@ -110,6 +124,10 @@ contract StakeRewarder is Ownable {
 
 		// Approve the vault to pull reward tokens
 		_rewardToken.approve(address(_vault), 2**256 - 1);
+
+		token = _token;
+		_setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+		_setupRole(ISSUER, msg.sender);
 	}
 
 	/**
@@ -497,6 +515,257 @@ contract StakeRewarder is Ownable {
 			vestingDuration, // uint256 _duration,
 			0 // uint256 _initialPct
 		);
+
+		return amount;
+	}
+
+	/**
+	 * @dev Creates a new allocation for a beneficiary. Tokens are released
+	 * linearly over time until a given number of seconds have passed since the
+	 * start of the vesting schedule. Callable only by issuers.
+	 * @param _beneficiary The address to which tokens will be released
+	 * @param _amount The amount of the allocation (in wei)
+	 * @param _startAt The unix timestamp at which the vesting may begin
+	 * @param _cliff The number of seconds after _startAt before which no vesting occurs
+	 * @param _duration The number of seconds after which the entire allocation is vested
+	 * @param _initialPct The percentage of the allocation initially available (integer, 0-100)
+	 */
+	function issue(
+		address _beneficiary,
+		uint256 _amount,
+		uint256 _startAt,
+		uint256 _cliff,
+		uint256 _duration,
+		uint256 _initialPct
+	) external override onlyRole(ISSUER) {
+		require(
+			token.allowance(msg.sender, address(this)) >= _amount,
+			"Token allowance not sufficient"
+		);
+		require(
+			_beneficiary != address(0),
+			"Cannot grant tokens to the zero address"
+		);
+		require(_cliff <= _duration, "Cliff must not exceed duration");
+		require(
+			_initialPct <= 100,
+			"Initial release percentage must be an integer 0 to 100 (inclusive)"
+		);
+
+		// Pull the number of tokens required for the allocation.
+		token.safeTransferFrom(msg.sender, address(this), _amount);
+
+		// Increase the total pending for the address.
+		pendingAmount[_beneficiary] = pendingAmount[_beneficiary].add(_amount);
+
+		// Push the new allocation into the stack.
+		userAllocations[_beneficiary].push(
+			Allocation({
+				claimed: 0,
+				cliff: _cliff,
+				duration: _duration,
+				initial: _amount.mul(_initialPct).div(100),
+				start: _startAt,
+				total: _amount
+			})
+		);
+
+		emit Issued(
+			_beneficiary,
+			userAllocations[_beneficiary].length - 1,
+			_amount,
+			_startAt,
+			_cliff,
+			_duration
+		);
+	}
+
+	/**
+	 * @dev Revokes an existing allocation. Any unclaimed tokens are recalled
+	 * and sent to the caller. Callable only be issuers.
+	 * @param _beneficiary The address whose allocation is to be revoked
+	 * @param _id The allocation ID to revoke
+	 */
+	function revoke(address _beneficiary, uint256 _id)
+		external
+		override
+		onlyRole(ISSUER)
+	{
+		Allocation storage allocation = userAllocations[_beneficiary][_id];
+
+		// Calculate the remaining amount.
+		uint256 total = allocation.total;
+		uint256 remainder = total.sub(allocation.claimed);
+
+		// Update the total pending for the address.
+		pendingAmount[_beneficiary] = pendingAmount[_beneficiary].sub(
+			remainder
+		);
+
+		// Update the allocation to be claimed in full.
+		allocation.claimed = total;
+
+		// Transfer the tokens vested
+		token.safeTransfer(msg.sender, remainder);
+		emit Revoked(_beneficiary, _id, total, remainder);
+	}
+
+	/**
+	 * @dev Transfers vested tokens from an allocation to its beneficiary. Callable by anyone.
+	 * @param _beneficiary The address that has vested tokens
+	 * @param _id The vested allocation index
+	 */
+	function release(address _beneficiary, uint256 _id) public {
+		Allocation storage allocation = userAllocations[_beneficiary][_id];
+
+		// Calculate the releasable amount.
+		uint256 amount = _releasableAmount(allocation);
+		require(amount > 0, "Nothing to release");
+
+		// Add the amount to the allocation's total claimed.
+		allocation.claimed = allocation.claimed.add(amount);
+
+		// Subtract the amount from the beneficiary's total pending.
+		pendingAmount[_beneficiary] = pendingAmount[_beneficiary].sub(amount);
+
+		// Transfer the tokens to the beneficiary.
+		token.safeTransfer(_beneficiary, amount);
+
+		emit Released(
+			_beneficiary,
+			_id,
+			amount,
+			allocation.total.sub(allocation.claimed)
+		);
+	}
+
+	/**
+	 * @dev Transfers vested tokens from any number of allocations to their beneficiary. Callable by anyone. May be gas-intensive.
+	 * @param _beneficiary The address that has vested tokens
+	 * @param _ids The vested allocation indexes
+	 */
+	function releaseMultiple(address _beneficiary, uint256[] calldata _ids)
+		external
+		override
+	{
+		for (uint256 i = 0; i < _ids.length; i++) {
+			release(_beneficiary, _ids[i]);
+		}
+	}
+
+	/**
+	 * @dev Gets the number of allocations issued for a given address.
+	 * @param _beneficiary The address to check for allocations
+	 */
+	function allocationCount(address _beneficiary)
+		public
+		view
+		returns (uint256 count)
+	{
+		return userAllocations[_beneficiary].length;
+	}
+
+	/**
+	 * @dev Calculates the amount that has already vested but has not yet been released for a given address.
+	 * @param _beneficiary Address to check
+	 * @param _id The allocation index
+	 */
+	function releasableAmount(address _beneficiary, uint256 _id)
+		public
+		view
+		returns (uint256 amount)
+	{
+		Allocation memory allocation = userAllocations[_beneficiary][_id];
+		return _releasableAmount(allocation);
+	}
+
+	/**
+	 * @dev Gets the total releasable for a given address. Likely gas-intensive, not intended for contract use.
+	 * @param _beneficiary Address to check
+	 */
+	function totalReleasableAount(address _beneficiary)
+		external
+		view
+		override
+		returns (uint256 amount)
+	{
+		for (uint256 i = 0; i < allocationCount(_beneficiary); i++) {
+			amount = amount.add(releasableAmount(_beneficiary, i));
+		}
+		return amount;
+	}
+
+	/**
+	 * @dev Calculates the amount that has vested to date.
+	 * @param _beneficiary Address to check
+	 * @param _id The allocation index
+	 */
+	function vestedAmount(address _beneficiary, uint256 _id)
+		public
+		view
+		returns (uint256)
+	{
+		Allocation memory allocation = userAllocations[_beneficiary][_id];
+		return _vestedAmount(allocation);
+	}
+
+	/**
+	 * @dev Gets the total ever vested for a given address. Likely gas-intensive, not intended for contract use.
+	 * @param _beneficiary Address to check
+	 */
+	function totalVestedAmount(address _beneficiary)
+		external
+		view
+		override
+		returns (uint256 amount)
+	{
+		for (uint256 i = 0; i < allocationCount(_beneficiary); i++) {
+			amount = amount.add(vestedAmount(_beneficiary, i));
+		}
+		return amount;
+	}
+
+	/**
+	 * @dev Calculates the amount that has already vested but hasn't been released yet.
+	 * @param allocation Allocation to calculate against
+	 */
+	function _releasableAmount(Allocation memory allocation)
+		internal
+		view
+		returns (uint256)
+	{
+		return _vestedAmount(allocation).sub(allocation.claimed);
+	}
+
+	/**
+	 * @dev Calculates the amount that has already vested.
+	 * @param allocation Allocation to calculate against
+	 */
+	function _vestedAmount(Allocation memory allocation)
+		internal
+		view
+		returns (uint256 amount)
+	{
+		if (block.timestamp < allocation.start.add(allocation.cliff)) {
+			// Nothing is vested until after the start time + cliff length.
+			amount = 0;
+		} else if (
+			block.timestamp >= allocation.start.add(allocation.duration)
+		) {
+			// The entire amount has vested if the entire duration has elapsed.
+			amount = allocation.total;
+		} else {
+			// The initial tranche is available once the cliff expires, plus any portion of
+			// tokens which have otherwise become vested as of the current block's timestamp.
+			amount = allocation.initial.add(
+				allocation
+					.total
+					.sub(allocation.initial)
+					.sub(amount)
+					.mul(block.timestamp.sub(allocation.start))
+					.div(allocation.duration)
+			);
+		}
 
 		return amount;
 	}
